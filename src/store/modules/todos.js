e0b0e370ede421a7d5471e0db457c4bc124bf0bb
José Hernández Rizo <josinhr>
2020-07-13 23:42:08 +0200

    INITIAL COMMIT

---
+++ src/store/modules/todos.js
@@ -0,0 +1,30 @@
+//import axios from 'axios';
+
+import Axios from "axios";
+
+const state = {
+    todos: []
+};
+
+const getters = {
+    allTodos: (state) => state.todos
+};
+
+const actions = {
+    async fetchTodos({commit}){
+        const response  = await Axios.get('https://jsonplaceholder.typicode.com/todos');
+
+        commit('setTodos',response.data)
+    }
+};
+
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos)
+};
+
+export default{
+    state,
+    getters,
+    actions,
+    mutations
+};
